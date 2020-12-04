a7f35896b0cd04fb021408910151d42e729a4df4
Tohid <tohid.yagmuri@gmail.com>
2020-12-04 21:47:51 +0330

    Uploading Project

---
+++ src/main/java/com/ticketing/barobax/BarobaxTicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ticketing.barobax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarobaxTicketingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarobaxTicketingSystemApplication.class, args);
+    }
+
+}
